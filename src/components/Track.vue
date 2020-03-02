bcfeb1e410318cef5149e4bd753d386843d85625
Zoltan Flamis <flamisz@gmail.com>
2020-03-02 14:44:24 +1300

    svg tracks

---
+++ src/components/Track.vue
@@ -0,0 +1,14 @@
+<template>
+	<svg class="flex-shrink-0 -ml-1 mr-3 h-6 w-6 text-gray-400 group-focus:text-gray-500 transition ease-in-out duration-150" stroke="currentColor" fill="none" viewBox="0 0 20 20">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" :d="path" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'Track',
+  props: {
+    path: String
+  }
+}
+</script>
--- src/races.js
+++ src/races.js
@@ -9,7 +9,8 @@ const races = {
     },
     tz: "Australia/Melbourne",
     circuit_id: "albert_park",
-    name: "australia"
+    name: "australia",
+    svg_path: "M2 9C2 8.66667 2.3 7.5 3.5 5.5L6 5L8 7L7.5 7.5L8.5 9.5L9.5 11H12L12.5 10.5H16L18 12.5L16.5 14.5L15 13.5L14 15L7.5 14V13L2.5 11.5V10.5L2 9Z"
   },
 
   'bahrain': {
@@ -22,7 +23,8 @@ const races = {
     },
     tz: "Asia/Bahrain",
     circuit_id: "bahrain",
-    name: "bahrain"
+    name: "bahrain",
+    svg_path: "M3.5 12L4.5 5L7.5 7.5L8 8V9.5L10.5 11L10 11.5L6 11L5.5 12L14 12.5L14.5 11.5L14 10.5L12 9.5L11.5 8.5L12.5 6H13L18 14L17 14.5H3L4 13.5L3.5 12Z"
   },
 
   'china': {
@@ -35,7 +37,8 @@ const races = {
     },
     tz: "Asia/Shanghai",
     circuit_id: "shanghai",
-    name: "china"
+    name: "china",
+    svg_path: "M9.5 4.5L6 14L1.5 14.5L1 15.5H17.5L18.5 15L19 14L18 12.5L17 13V14H11L10.5 12L11.6875 12.5L13 12V11L12 9L12.5 7.5H17.5L18 6.5L14.5 5.5L11 6.25L10 5.5V5H10.5L11.5 5.5L11.6875 5V4.5L11 4H10L9.5 4.5Z"
   },
 
   'azerbaijan': {
@@ -48,7 +51,8 @@ const races = {
     },
     tz: "Asia/Baku",
     circuit_id: "BAK",
-    name: "azerbaijan"
+    name: "azerbaijan",
+    svg_path: "M10 9.5L6.5 11.5L4 15L1 13.5V12L2 10.5L4.5 9L6 9.5L6.5 10.5L9 9L13.5 7L13 5.5L18 4L19 6.5L10 9.5Z"
   },
 
   'spain': {
@@ -61,7 +65,8 @@ const races = {
     },
     tz: "Europe/Madrid",
     circuit_id: "catalunya",
-    name: "spain"
+    name: "spain",
+    svg_path: "M4 12H18L19 11L18.5 10V8L16 7L15.5 7.5V8.5L16.5 9L17 10.5L10.5 7H10L8 10V11H5.5L3 9.5L3.5 9L6 8.5L7 8V7H3L1.5 7.5L1 9L1.5 10L3 10.5L3.5 11.5L4 12Z"
   },
 
   'monaco': {
@@ -74,7 +79,8 @@ const races = {
     },
     tz: "Europe/Monaco",
     circuit_id: "monaco",
-    name: "monaco"
+    name: "monaco",
+    svg_path: "M1 12V14V15.5L2.5 16V15.5L2 14V12.5H3L3.5 10L3 9.5L4 8.5L5 7.5L9 9.5L9.5 9L10.5 9.5L13.5 10L15 9.5L16.5 9L19 6L18.5 5.5L18 5L17.5 6L17 5V4L13.5 6V6.5L14 7.5L13.5 8.5H11.5L9 8L4.5 6.5L4 7L2 9.5L1 12Z"
   },
 
   'canada': {
@@ -87,7 +93,8 @@ const races = {
     },
     tz: "America/Montreal",
     circuit_id: "villeneuve",
-    name: "canada"
+    name: "canada",
+    svg_path: "M3.5 7.5L7.5 9.5L8 10.5L9 11L14.5 13.5L17 14.5H18.5L15 11.5L14.5 10L13.5 8.5L10 6L9 6.5L7.5 5.5H5L4 5L1 6L1.5 7L2 6.5L3.5 7.5Z"
   },
 
   'france': {
@@ -100,7 +107,8 @@ const races = {
     },
     tz: "Europe/Paris",
     circuit_id: "ricard",
-    name: "france"
+    name: "france",
+    svg_path: "M3 5L10.5 8L11 8.5L12 8L13.5 9L18 11L19 12L18.5 14L18 15L17 14L18 13V12H15.5L14.5 11L14 11.5L13.5 12.5L7 7.5H5.5L2.5 6L1 5L1.5 4.5L2 4H2.5V4.5L3 5Z"
   },
 
   'austria': {
@@ -113,7 +121,8 @@ const races = {
     },
     tz: "Europe/Vienna",
     circuit_id: "red_bull_ring",
-    name: "austria"
+    name: "austria",
+    svg_path: "M4 4L1 4.5L4 8L6.5 13L8.5 15.5L16.5 13.5L19 12.5L17.5 9.5H10.5L9 11L8 11.5L7 11L6 8.5V7.5L7.5 7L10 7.5L12 7L12.5 5.5L4 4Z"
   },
 
   'great-britain': {
@@ -126,7 +135,8 @@ const races = {
     },
     tz: "Europe/London",
     circuit_id: "silverstone",
-    name: "great-britain"
+    name: "great-britain",
+    svg_path: "M2 4L1 9L3.5 11.5L4.5 10.5L3.5 10L3 9.25L8.5 5.1875H9L10.5 6.5H8.8125L10 8.5L9 11L9.5 12L13.5 16L14.5 15.5V14.5L17 13H18L19 12L12.5 5.5L11.5 4H10.5L8.8125 3L7.5 4L4 3L3 3.5L2 4Z"
   },
 
   'germany': {
@@ -139,7 +149,8 @@ const races = {
     },
     tz: "Europe/Berlin",
     circuit_id: "hockenheimring",
-    name: "germany"
+    name: "germany",
+    svg_path: "M2.5 7L1 9L3.5 15L5.5 14L4 12.5L3.5 11L4.5 10.5L7.5 13L11.5 10.5V8.5L14.5 9L18.5 5.5L14 7.5H11L8.5 7L6 5.5L5.5 4H5L2.5 7Z"
   },
 
   'hungary': {
@@ -152,7 +163,8 @@ const races = {
     },
     tz: "Europe/Budapest",
     circuit_id: "hungaroring",
-    name: "hungary"
+    name: "hungary",
+    svg_path: "M5.5 15.5H15V14H12.5V13H15.5L16 8.5L13.5 7.5L12 5.5L11 6L9.5 5V4.5L7 4L6 4.5V5.5L7.5 6.5V7.5L8.5 12.5L10.5 13.5L10 14H8L5 15L5.5 15.5Z"
   },
 
   'belgium': {
@@ -165,7 +177,8 @@ const races = {
     },
     tz: "Europe/Brussels",
     circuit_id: "spa",
-    name: "belgium"
+    name: "belgium",
+    svg_path: "M6 9.5L15 5H17L18.5 6.77596V7.5L17 6.77596L12.5 8.5L13 10L17 11.5L16.5 13L18.5 14.5L17 15.5L15 15L13 12L11 11L9.5 12L6 12.5L5.5 12L2.5 14L1 14.5L1.5 13.5L4 10.5L5 9.5H6Z"
   },
 
   'italy': {
@@ -178,7 +191,8 @@ const races = {
     },
     tz: "Europe/Rome",
     circuit_id: "monza",
-    name: "italy"
+    name: "italy",
+    svg_path: "M3.5 5L1 5.5L4 13.5L8 14L18.5 13.5L19 12L9.5 11L3.5 5Z"
   },
 
   'singapore': {
@@ -191,7 +205,8 @@ const races = {
     },
     tz: "Asia/Singapore",
     circuit_id: "marina_bay",
-    name: "singapore"
+    name: "singapore",
+    svg_path: "M18 12L17 13H14V11.5H12.5L12 13L9.5 12L8.5 11L5.5 9.5L3.5 15L2.5 13.5L2 12.5L1 12L4 7L5.5 8.5L7 6.5L10.5 9L16.5 9.5L15 6.5L16 5L16.5 5.5L17.5 6L18 12Z"
   },
 
   'russia': {
@@ -204,7 +219,8 @@ const races = {
     },
     tz: "Europe/Moscow",
     circuit_id: "sochi",
-    name: "russia"
+    name: "russia",
+    svg_path: "M16.5 10.5L19 6L17.5 5L16 7.5L16.5 8.5L15 9.5L14 9L12.5 9.5L7 7.5L4 8.5L4.5 10.5L4 11.5L1 12V14L2 15.5L6 14L5 12L5.5 10.5L6.5 10L8.5 10.5L9.5 12.5L16.5 10.5Z"
   },
 
   'japan': {
@@ -217,7 +233,8 @@ const races = {
     },
     tz: "Asia/Tokyo",
     circuit_id: "suzuka",
-    name: "japan"
+    name: "japan",
+    svg_path: "M10 15H2L1 13.5L2 13L3.5 13.5L5 12.5L6 13L7.5 12.5L9 13.5L10 12.5V9L11 8.5L14 10V10.5L14.5 7L18.5 5L19.5 5.5L19 7.5H13.5L11 10V13L10 15Z"
   },
 
   'mexico': {
@@ -230,7 +247,8 @@ const races = {
     },
     tz: "America/Mexico_City",
     circuit_id: "rodriguez",
-    name: "mexico"
+    name: "mexico",
+    svg_path: "M16 4L1 10V12.5L3 11.5L3.5 13L8 11H10.5L11.5 11.5L13.5 11L14.5 12H15.5L17.5 15.7037L18.5 14L17.5 13.5V5.5L16.5 5L16 4Z"
   },
 
   'united-states': {
@@ -243,7 +261,8 @@ const races = {
     },
     tz: "America/Chicago",
     circuit_id: "americas",
-    name: "united-states"
+    name: "united-states",
+    svg_path: "M18 4L7.5 8L8.5 9.5L8 10L7 8.5L6 9L7.5 11V11.5L5.5 12L3.5 10.5L1 12L8.5 16.5L7.5 13.5L9.5 11.5V10.5H10.5L11 8.5H13L14 7.5L15.5 8.5L17 7.5L18 4Z"
   },
 
   'brazil': {
@@ -256,7 +275,8 @@ const races = {
     },
     tz: "America/Sao_Paulo",
     circuit_id: "interlagos",
-    name: "brazil"
+    name: "brazil",
+    svg_path: "M4 15.5H15V12L8.5 9L9 7L12 6L11.5 8L12.5 8.5L14 6.5L16 6L14.5 8.5L15 10L19 11L19.5 9.5L18 6L16 5L10 5.5L1 10.5L3 11.5V14.5L4 15.5Z"
   },
 
   'abu-dhabi': {
@@ -269,7 +289,8 @@ const races = {
     },
     tz: "Asia/Dubai",
     circuit_id: "yas_marina",
-    name: "abu-dhabi"
+    name: "abu-dhabi",
+    svg_path: "M18.5 4H5L4.5 5L2 9L1 12.5V14.5L2 15L3.5 14.5L2.5 11.5L3 10L4 9.5L4.5 10.5L6 9.5L4.5 6.5L6.5 5L9.5 10.5L12 9.5V7.5L12.5 6.5H14L18.5 4Z"
   }
 }
 
