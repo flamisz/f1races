a39674bf3aac17137d8da97be8ddb42b2cc926da
Zoltan Flamis <zoltan@hey.com>
2021-04-10 09:35:37 +1200

    menu with races

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,11 +1,17 @@
 <template>
 <div>
-    Nav
+    <router-link v-for="(race, name) in races" :key="name" :to="'/race/' + name">
+        <div class="flex-row">
+            <div>{{ name }}</div>
+            <div>{{ raceDate(race) }}</div>
+        </div>
+    </router-link>
 </div>
 </template>
 
 <script>
 import { getRaces } from '@/races'
+import moment from 'moment-timezone'
 
 export default {
     name: 'Nav',
@@ -24,6 +30,14 @@ export default {
         fetchData () {
             this.races = getRaces()
         },
+
+        raceDate: function (race) {
+            if (moment(race.times.from).month() == moment(race.times.to).month()) {
+                return moment(race.times.from).format('DD') + '-' + moment(race.times.to).format('DD') + ' ' + moment(race.times.from).format('MMM').toLowerCase()
+            }
+
+            return moment(race.times.from).format('DD') + ' ' + moment(race.times.from).format('MMM').toLowerCase() + '-' + moment(race.times.to).format('DD') + ' ' + moment(race.times.from).format('MMM').toLowerCase()
+        }
     }
 }
 </script>
